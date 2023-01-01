1bc7162fff8b110fe79fd4ffe1b09a4769f07123
Guillaume DUALE <guillaume.duale@virtual-expo.com>
2023-01-01 19:46:25 +0100

    suite

--- play_music.sh
+++ play_music.sh
@@ -1,10 +1,12 @@
 #!/bin/bash 
 
-set -x
 # on met une carte : ça joue la musique
 # si on retire cette carte la musique s'arrête
 
 current_code=""
+dossier=$(dirname $(readlink -f "$0"))
+
+
 while true
 do
   code=$(nfc-list|grep UID|awk -F ':' '{print $2}'|sed 's/ //g')
@@ -20,13 +22,33 @@ do
   if [[ $code != "" ]] && [[ $player == "" ]]; then
     if [ $code == "e2ef8d0d" ];then 
       current_code="e2ef8d0d"
-      mplayer -really-quiet "/home/gui/Musique/Ultrasyd - Atari Sucks.mp3" &
+      mplayer -really-quiet "${dossier}/mp3/Ahlescrocodiles.mp3" &
     fi
     
     if [ $code == "d62caa1a" ];then 
       current_code="d62caa1a"
-     mplayer -really-quiet "/home/gui/Musique/Ultrasyd-Croissant[BLEEPSTREET]320kbps.mp3" &
+     mplayer -really-quiet "mp3/Bateau-sur-leau.mp3" &
     fi
-  fi  
+    
+    if [ $code == "d674101e" ];then 
+      current_code="d674101e"
+     mplayer -really-quiet "mp3/Dans-sa-maison-un-grand-cerf.mp3" &
+    fi
+    
+    if [ $code == "d6bfc61a" ];then 
+      current_code="d6bfc61a"
+     mplayer -really-quiet "mp3/GerardDaltonlasorciereGrabouilla.mp3" &
+    fi
+    
+    if [ $code == "62262d95" ];then 
+      current_code="62262d95"
+     mplayer -really-quiet "mp3/LuisFonsi-Despacitoft.DaddyYankee.mp3" &
+    fi
+    
+    if [ $code == "e695c41a" ];then 
+      current_code="e695c41a"
+     mplayer -really-quiet "mp3/PeppaPigFrancais-BingBongZoo.mp3" &
+    fi
+  fi
   sleep 1
 done
