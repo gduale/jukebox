5eb89a4849c7751d955974eda1e7cb70e91778a1
tazou <guillaume.duale@gmail.com>
2023-01-02 00:29:11 +0100

    Update play_music.sh
    dossier

--- play_music.sh
+++ play_music.sh
@@ -27,27 +27,27 @@ do
     
     if [ $code == "d62caa1a" ];then 
       current_code="d62caa1a"
-     mplayer -really-quiet "mp3/Bateau-sur-leau.mp3" &
+     mplayer -really-quiet "${dossier}/mp3/Bateau-sur-leau.mp3" &
     fi
     
     if [ $code == "d674101e" ];then 
       current_code="d674101e"
-     mplayer -really-quiet "mp3/Dans-sa-maison-un-grand-cerf.mp3" &
+     mplayer -really-quiet "${dossier}/mp3/Dans-sa-maison-un-grand-cerf.mp3" &
     fi
     
     if [ $code == "d6bfc61a" ];then 
       current_code="d6bfc61a"
-     mplayer -really-quiet "mp3/GerardDaltonlasorciereGrabouilla.mp3" &
+     mplayer -really-quiet "${dossier}/mp3/GerardDaltonlasorciereGrabouilla.mp3" &
     fi
     
     if [ $code == "62262d95" ];then 
       current_code="62262d95"
-     mplayer -really-quiet "mp3/LuisFonsi-Despacitoft.DaddyYankee.mp3" &
+     mplayer -really-quiet "${dossier}/mp3/LuisFonsi-Despacitoft.DaddyYankee.mp3" &
     fi
     
     if [ $code == "e695c41a" ];then 
       current_code="e695c41a"
-     mplayer -really-quiet "mp3/PeppaPigFrancais-BingBongZoo.mp3" &
+     mplayer -really-quiet "${dossier}/mp3/PeppaPigFrancais-BingBongZoo.mp3" &
     fi
   fi
   sleep 1
